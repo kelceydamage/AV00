21f41cad1a468ffa41b3a3411e836e04953c73df
kelcey Jamison-Damage <kelcey@kelceydamage.com>
2023-06-01 14:51:50 -0700

    integrate System.Device.Gpio assemblies

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.IO;
+using System.Device.I2c;
+
+namespace sensors_test
+{
+    public class Program
+    {
+        private static readonly int busId = 8;
+        private static readonly int deviceAddress = 0x1c;
+
+        public static void Main()
+        {
+            I2cConnectionSettings I2CConnectionSettings = new(busId, deviceAddress);
+            I2cDevice I2CDevice = I2cDevice.Create(I2CConnectionSettings);
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
