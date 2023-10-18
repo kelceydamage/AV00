937d7c3ffbde285e41dd365486bf9b18530df8f6
kelcey Jamison-Damage <kelcey@kelceydamage.com>
2023-10-18 15:03:59 -0700

    Migrated MotorCommandEventModel to it's own file.

---
+++ Controllers/MotorController/MotorCommandEventModel.cs
@@ -0,0 +1,37 @@
+﻿using AV00_Shared.FlowControl;
+using AV00_Shared.Models;
+using System.Device.Gpio;
+using System.Text.Json.Serialization;
+
+namespace AV00.Controllers.MotorController
+{
+    [Serializable]
+    public class MotorCommandEventModel : EventModel, IEventModel
+    {
+        public PinValue Direction { get => direction; }
+        private readonly PinValue direction;
+        public ushort PwmAmount { get => pwmAmount; }
+        private readonly ushort pwmAmount;
+        public EnumMotorCommands Command { get => command; }
+        private readonly EnumMotorCommands command;
+        public EnumExecutionMode Mode { get => mode; }
+        private readonly EnumExecutionMode mode;
+
+        [JsonConstructor]
+        public MotorCommandEventModel(
+            string ServiceName,
+            EnumMotorCommands Command,
+            PinValue Direction,
+            ushort PwmAmount,
+            EnumExecutionMode Mode = EnumExecutionMode.Blocking,
+            Guid? Id = null,
+            string? TimeStamp = null
+        ) : base(ServiceName, Id, TimeStamp)
+        {
+            command = Command;
+            direction = Direction;
+            pwmAmount = PwmAmount;
+            mode = Mode;
+        }
+    }
+}
--- Controllers/MotorController/MotorControllers.cs
+++ Controllers/MotorController/MotorControllers.cs
@@ -1,9 +1,6 @@
 ﻿using System.Device.Gpio;
-using System.Text.Json.Serialization;
 using AV00.Drivers.Motors;
 using AV00.Drivers.IO;
-using AV00_Shared.FlowControl;
-using AV00_Shared.Models;
 
 namespace AV00.Controllers.MotorController
 {
@@ -38,36 +35,6 @@ namespace AV00.Controllers.MotorController
     {
         Move,
         Turn,
-    }
-
-    [Serializable]
-    public class MotorCommandEventModel : BaseModel, IEventModel
-    {
-        public PinValue Direction { get => direction; }
-        private readonly PinValue direction;
-        public ushort PwmAmount { get => pwmAmount; }
-        private readonly ushort pwmAmount;
-        public EnumMotorCommands Command { get => command; }
-        private readonly EnumMotorCommands command;
-        public EnumExecutionMode Mode { get => mode; }
-        private readonly EnumExecutionMode mode;
-
-        [JsonConstructor]
-        public MotorCommandEventModel(
-            string ServiceName, 
-            EnumMotorCommands Command, 
-            PinValue Direction, 
-            ushort PwmAmount, 
-            EnumExecutionMode Mode = EnumExecutionMode.Blocking, 
-            Guid? Id = null,
-            string? TimeStamp = null
-        ) : base(ServiceName, Id, TimeStamp)
-        {
-            command = Command;
-            direction = Direction;
-            pwmAmount = PwmAmount;
-            mode = Mode;
-        }
     }
 
     internal class PDSGBGearboxMotorController: IMotorController
